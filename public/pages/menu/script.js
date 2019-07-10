c6c560e5e7a0fa354a349c56c47c7931f580bb9e
thanhthinh95 <thanhthinh95@hoasao.vn>
2019-07-10 09:02:52 +0700

    update view menu

--- public/pages/menu/script.js
+++ public/pages/menu/script.js
@@ -1,91 +1,47 @@
 var eventPage = function($) {
     function bindEventClick() {
-        $(document).on('click', '#config', function (e) {
-            $('#modal_config').modal('show');
-
-            $("#table_modal tbody").empty();
-            $('#sumRow').val(_sumRow);
-            $('#sum_column').text(_.filter(_fields, {statusShow : -1}))
-            _appendModalConfig('#table_modal', _.filter(_fields, {statusShow : -1}));
-            _appendModalConfig('#table_modal', _.filter(_fields, {statusShow : 1}));
-            _appendModalConfig('#table_modal', _.filter(_fields, {statusShow : 0}));
-        })
-
-        $(document).on('submit', '#form_modal_config', function (e) {
-            e.preventDefault();
-            var _filedTems = _.filter(_fields, {statusShow : -1});
-            let objChecked = $('#form_modal_config').serializeArray();
-            
-            let objSumRow = _.find(objChecked, {name : 'sumRow'}); //Tim kiem va dat lai sumRow mac dinh cua page
-            _sumRow = objSumRow ? objSumRow.value  : _sumRow;
-
-            _.forEach(objChecked, function(item) {//Tim kiem va data lai gia tri fields
-                let nameItems = _.split(item.name, 'field_config_');
-                if(nameItems[1]){//Da tim thay fields can dat gia tri
-                    let obj = _.find(_fields, {path : nameItems[1]});
-                    obj.statusShow = 1;
-                    _filedTems.push(obj);
-                }
-            })
-
-            let objUnCheck = _.map($("input:checkbox:not(:checked)"), function (item) {
-                return {name : item.name};
-            })
-
-            _.forEach(objUnCheck, function(item) {//Tim kiem va dat lai data Uncheck
-                let nameItems = _.split(item.name, 'field_config_');
-                if(nameItems[1]){//Da tim thay fields can dat gia tri
-                    let obj = _.find(_fields, {path : nameItems[1]});
-                    obj.statusShow = 0;
-                    _filedTems.push(obj);
-                }
-            })
-            
-            _fields = _filedTems;
-            $('#modal_config').modal('hide');
-            bindHeadTable();
-            bindBodyTable();
-        })
 
 
-        $(document).on('submit', '#form_table', function name(e) {
-            e.preventDefault();
-            bindBodyTable();
-        })
 
-        $(document).on('click', '.sort' , function(e){
-            _changeIconSort($(this));
-            bindBodyTable();
-        });
+
     }
 
- 
-    function bindHeadTable() {
-        _bindHeadTable('#table_data', _fields);
-        $('select').selectpicker({
-            countSelectedText :   '{0} mục đã chọn',
-        });
-
-        $('.datetimepicker-input').datetimepicker({
-            locale : 'vi',
-            format : 'DD/MM/YYYY'
-        });
-    }
-    
 
     function bindBodyTable() {
-        var objFilter = {
-            dataMatch : _createObjectInForm('#form_table'),
-            dataSort : _createObjectSort(), 
-        }
-     
-        _AjaxObject('/user/search', 'GET', objFilter, function(resp) {
-            if(resp.code == 200){
-                _bindBodyTable('#form_table', _fields, resp.data);
-            }else{
-                _DialogError(resp.mesage);
-            }
+        
+        $('#table_data tbody').empty();
+
+        _.forEach(_menus, function (menu) {
+            console.log(menu);
+            
+            var html = '<tr>';
+            html +=  '<td class="text-center align-middle">' +
+                    (menu.icon ?  '<i class="'+ menu.icon +'" aria-hidden="true"></i>' : '')
+                    '</td>';
+
+            html += '<td><span>' + menu.name + '</span></td>';
+            html += '<td><span>' + (menu.path ? menu.path : "") + '</span></td>';
+            html += '<td class="text-center align-middle"><span class="">' +
+                    '<i class="fa fa-pencil-square-o text-primary pr-3" aria-hidden="true" data-toggle="tooltip" title="Chỉnh sửa"></i>' + 
+                    '<i class="fa fa-minus-square text-danger" aria-hidden="true" data-toggle="tooltip" title="Loại bỏ"></i>' +
+                    '</span></td>';
+                             
+            html += '</tr>'
+            $('#table_data tbody').append(html);
+
+
+            
         })
+
+        // $('#my-sortable').sortable();
+
+        $('#table_data tbody').sortable({
+            connectWith: '#table_data tbody'
+        }).disableSelection();
+
+        
+
+
     }
 
 
@@ -93,10 +49,9 @@ var eventPage = function($) {
         init : function () {
             console.log('dang thuc hien init su kien menu');
             $('select').selectpicker();
+            // $('#table_data tbody').sortable();
             
-            // $('#table_modal tbody').sortable();
-            // bindHeadTable();
-            // bindBodyTable();
+            bindBodyTable();
             // bindEventClick();
         },
         uncut : function (){
